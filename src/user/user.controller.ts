c65e48701353f08b7aa5c573b8e81a93a551a167
seanlin1016@gmail.com <seanlin1016@gmail.com>
2022-05-29 15:52:13 +0800

    create custom parameter decorator for getting user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get, Req, UseGuards } from "@nestjs/common";
-import { Request } from "express";
+import { User } from "@prisma/client";
+import { GetUser } from "src/auth/decorators";
 import { JwtGuard } from "src/auth/guard";
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
 
-    @UseGuards(JwtGuard)
     @Get('details')
-    getUserDetails(@Req() req: Request) {
-        return req.user;
+    getUserDetails(@GetUser() user: User, @GetUser('email') email: string) {
+        console.log("get user email: ", email)
+        return user;
     }
 }
