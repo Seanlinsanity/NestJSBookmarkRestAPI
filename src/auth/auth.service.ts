6d90eaa4422b00e1c61906c5bb82f8fa3aeabda4
seanlin1016@gmail.com <seanlin1016@gmail.com>
2022-05-29 15:39:22 +0800

    use JWT auth guard to check api authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,18 @@ import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AuthService {
-    constructor(private primsa: PrismaService) {}
+    constructor(
+        private primsa: PrismaService, 
+        private jwtService: JwtService,
+        private config: ConfigService
+    ) {}
 
-    async signup(dto: AuthDto): Promise<AuthDto> {
+    async signup(dto: AuthDto): Promise<{access_token: string}> {
         const hash = await argon.hash(dto.password)
 
         const user = await this.primsa.user.create({
@@ -16,13 +22,11 @@ export class AuthService {
                 password: hash
             },
         });
-        
-        delete user.password
 
-        return user
+        return this.signToken(user.id, user.email)
     }
 
-    async login(dto: AuthDto): Promise<AuthDto> {
+    async login(dto: AuthDto): Promise<{access_token: string}> {
         const user = await this.primsa.user.findUnique({
             where: {
                 email: dto.email,
@@ -39,7 +43,24 @@ export class AuthService {
             throw new ForbiddenException('Incorrect Credentials...')
         }
 
-        delete user.password
-        return user
+        return this.signToken(user.id, user.email)
+    }
+
+    async signToken(
+        userId: number, 
+        email: string
+    ): Promise<{access_token: string}> {
+        const payload = {
+            sub: userId,
+            email
+        }
+        const token = await this.jwtService.signAsync(payload, {
+            expiresIn: '15m',
+            secret: this.config.get('JWT_SECRET')
+        })
+
+        return {
+            access_token: token
+        }
     }
 }
