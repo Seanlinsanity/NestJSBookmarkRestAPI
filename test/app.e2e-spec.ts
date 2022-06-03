cfa1ac5f7a6d7ad4030c854483363b203b0784c3
Sean Lin <seanlin1016@gmail.com>
2022-06-03 17:30:37 +0800

    set up clean Database function in prisma service

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { Test } from "@nestjs/testing"
+import { PrismaService } from "../src/prisma/prisma.service";
 import { AppModule } from "../src/app.module"
 
 describe("App e2e", () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -13,6 +15,9 @@ describe("App e2e", () => {
       whitelist: true
     }));
     await app.init();
+
+    prisma = app.get(PrismaService)
+    await prisma.cleanDB()
   });
 
   afterAll(() => {
